5acfc81c06bd8ca86ff6d0463399eb8a328b7ccf
DoyupK <gltlvl12@gmail.com>
2022-05-18 12:57:49 +0900

    택시 탑승 시 탑승정보 객체 생성 탑승정보 객체{ 택시정보, 이용자 정보, 목적지, 목적지 거리 }

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
 public class Main {
     public static void main(String[] args){
         Taxi taxi_1 = new Taxi(200,10,30,10);
+        Taxi taxi_2 = new Taxi(300,20,30,10);
         Passenger dyk = new Passenger("dyk",10000);
+        System.out.println(dyk.hasMoney);
         dyk.takeTaxi(dyk, taxi_1, "서울역",20);
         taxi_1.arrive();
+        System.out.println(dyk.hasMoney);
+        dyk.takeTaxi(dyk, taxi_2, "안양역", 20);
+        taxi_2.arrive();
         System.out.println(dyk.hasMoney);
     }
 }
